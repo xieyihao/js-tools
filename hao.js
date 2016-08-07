ddc7289b1c280d7b183b5240580a7bf5c34ecbcb
xieyihao lenovo gmail <xieyihaod@gmail.com>
2016-08-07 20:58:46 +0800

    1.添加判断string的方法

--- hao.js
+++ hao.js
@@ -205,3 +205,15 @@ Hao.regex.isPhoneNumber = function (phone) {
     }
     return false;
 };
+
+//验证是否是string
+function normalizeName(name) {
+	if (typeof name !== 'string') {
+	  name = String(name)
+	}
+	if (/[^a-z0-9\-#$%&'*+.\^_`|~]/i.test(name)) {
+	  throw new TypeError('Invalid character in header field name')
+	}
+	return name.toLowerCase()
+}
+
